ccf41f35fa58edcee842d524e519b53bd9c56027
Jellefish <JelleClignet@MacBook-Pro.local>
2015-04-15 23:31:05 +0200

    push

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,6 +28,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/noandroid', {
+        templateUrl: 'views/noandroid.html',
+        controller: 'NoandroidCtrl'
+      })
       .when('/verhaal', {
         templateUrl: 'views/verhaal.html',
         controller: 'VerhaalCtrl'
