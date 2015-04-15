1a51086f6b570406d92353d421a74f7dd87ce4f9
Jellefish <JelleClignet@MacBook-Pro.local>
2015-04-15 15:00:23 +0200

    PULL my finger

---
+++ app/scripts/controllers/verhaal-26.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cnmeApp.controller:Verhaal26Ctrl
+ * @description
+ * # Verhaal26Ctrl
+ * Controller of the cnmeApp
+ */
+angular.module('cnmeApp')
+  .controller('Verhaal26Ctrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
